b12c29307b8f0aa1c4ecb99fe05464693483bdce
manuelinho96 <manuelbonilla96@hotmail.com>
2018-05-15 13:45:39 -0400

    Anadida verificacion de clave
    Ahora se verifica que la clave cumpla con los requisitos especificados
    en la tarea.
    Agregado dos casos de prueba para validar una clave.

--- src/CasosDePrueba.py
+++ src/CasosDePrueba.py
@@ -19,19 +19,34 @@ class Test(unittest.TestCase):
 
     #Caso inicial para probar formato de email
     def test_emailIncorrecto(self):
-        self.emailvalido = self.prueba.registrarUsuario("hola", "s","s")
+        self.emailvalido = self.prueba.registrarUsuario("hola", "Manuel123","Manuel123")
         self.assertEqual(-1, self.emailvalido)
         
     #Caso inicial para probar cuando el email no contiene punto
     def test_emailIncorrecto2(self):
-        self.emailvalido = self.prueba.registrarUsuario("hola@s", "s","s")
+        self.emailvalido = self.prueba.registrarUsuario("hola@s", "Manuel123","Manuel123")
         self.assertEqual(-1, self.emailvalido)
     
     #Caso para probar que un email es valido
     def test_emailValido(self):
-        self.emailvalido = self.prueba.registrarUsuario("hola@s.com", "s","s")
+        self.emailvalido = self.prueba.registrarUsuario("hola@s.com", "Manuel123","Manuel123")
         self.assertEqual(0, self.emailvalido)
-
+        
+    # Caso interior claves desiguales
+    def test_clavesdesiguales(self):
+        self.emailvalido = self.prueba.registrarUsuario("hola@s.com", "Manuel123","Manuel12")
+        self.assertEqual(-2, self.emailvalido)
+    
+    # Caso interior claves iguales
+    def test_clavesiguales(self):
+        self.emailvalido = self.prueba.registrarUsuario("hola@s.com", "Manuel123","Manuel123")
+        self.assertEqual(0, self.emailvalido)
+    
+    # Caso interior la clave es invalida
+    def test_clavesinvalidas(self):
+        self.emailvalido = self.prueba.registrarUsuario("hola@s.com", "Ma2","Ma2")
+        self.assertEqual(-3, self.emailvalido)
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
--- src/SistLogin/models.py
+++ src/SistLogin/models.py
@@ -15,5 +15,23 @@ class Seguridad:
         except:
             print("Las claves no coinciden.")
             return -2
+        try:
+            minusculas = 0
+            mayusculas = 0
+            digitos = 0
+            assert(len(clave1) >= 8 and len(clave1) <= 16)
+            for char in clave1:
+                if char.isdigit():
+                    digitos += 1
+                elif char.isupper():
+                    mayusculas += 1
+                elif char.islower():
+                    minusculas += 1
+                else:
+                    raise
+            assert(digitos >= 1 and mayusculas >= 1 and minusculas >= 1 and mayusculas + minusculas >= 3)
+        except:
+            print("La clave no es valida")
+            return -3
         
         return 0
