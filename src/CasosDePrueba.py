19687c8e8efd9ee578fc70df2737c293e543e6b2
AuriCA32 <auri.git.17@gmail.com>
2018-05-15 14:57:50 -0400

    Agregado string para justificar rechazo de clave
    Ahora se imprime la razon por la cual se rechaza.
    Agregados casos frontera, esquina, y algunos maliciosos para la
    validación de la clave

--- src/CasosDePrueba.py
+++ src/CasosDePrueba.py
@@ -19,33 +19,84 @@ class Test(unittest.TestCase):
 
     #Caso inicial para probar formato de email
     def test_emailIncorrecto(self):
-        self.emailvalido = self.prueba.registrarUsuario("hola", "Manuel123","Manuel123")
-        self.assertEqual(-1, self.emailvalido)
+        self.validar = self.prueba.registrarUsuario("hola", "Manuel123","Manuel123")
+        self.assertEqual(-1, self.validar)
         
     #Caso inicial para probar cuando el email no contiene punto
     def test_emailIncorrecto2(self):
-        self.emailvalido = self.prueba.registrarUsuario("hola@s", "Manuel123","Manuel123")
-        self.assertEqual(-1, self.emailvalido)
+        self.validar = self.prueba.registrarUsuario("hola@s", "Manuel123","Manuel123")
+        self.assertEqual(-1, self.validar)
     
-    #Caso para probar que un email es valido
+    #Caso interior para probar que un email es valido
     def test_emailValido(self):
-        self.emailvalido = self.prueba.registrarUsuario("hola@s.com", "Manuel123","Manuel123")
-        self.assertEqual(0, self.emailvalido)
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel1234","Manuel1234")
+        self.assertEqual(0, self.validar)
         
     # Caso interior claves desiguales
     def test_clavesdesiguales(self):
-        self.emailvalido = self.prueba.registrarUsuario("hola@s.com", "Manuel123","Manuel12")
-        self.assertEqual(-2, self.emailvalido)
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel123","Manuel1234")
+        self.assertEqual(-2, self.validar)
     
     # Caso interior claves iguales
     def test_clavesiguales(self):
-        self.emailvalido = self.prueba.registrarUsuario("hola@s.com", "Manuel123","Manuel123")
-        self.assertEqual(0, self.emailvalido)
-    
-    # Caso interior la clave es invalida
-    def test_clavesinvalidas(self):
-        self.emailvalido = self.prueba.registrarUsuario("hola@s.com", "Ma2","Ma2")
-        self.assertEqual(-3, self.emailvalido)
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel123","Manuel123")
+        self.assertEqual(0, self.validar)
+        
+    #Caso frontera para cuando la clave es exactamente 8 caracteres
+    def test_clave8Char(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel12","Manuel12")
+        self.assertEqual(0, self.validar)
+        
+    #Caso frontera para cuando la clave es exactamente 16 caracteres
+    def test_clave16Char(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel1234567890","Manuel1234567890")
+        self.assertEqual(0, self.validar)  
+        
+    #Caso esquina para cuando la clave es exactamente 7 caracteres
+    def test_clave7Char(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel1","Manuel1")
+        self.assertEqual(-3, self.validar)  
+        
+    #Caso esquina para cuando la clave es exactamente 17 caracteres
+    def test_clave17Char(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel12345678901","Manuel12345678901")
+        self.assertEqual(-3, self.validar)  
+        
+    #Caso esquina para cuando la clave es exactamente 9 caracteres
+    def test_clave9Char(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel123","Manuel123")
+        self.assertEqual(0, self.validar)  
+        
+    #Caso esquina para cuando la clave es exactamente 15 caracteres
+    def test_clave15Char(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel123456789","Manuel123456789")
+        self.assertEqual(0, self.validar)
+        
+    #Caso malicioso cuando la clave no tiene digitos
+    def test_claveSinDigitos(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuelabc","Manuelabc")
+        self.assertEqual(-3, self.validar)
+        
+    #Caso malicioso cuando la clave no tiene mayusculas
+    def test_claveSinMayusculas(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "manuelab1","manuelab1")
+        self.assertEqual(-3, self.validar)
+        
+    #Caso malicioso cuando la clave no tiene minusculas
+    def test_claveSinMinusculas(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "MANUEL123","MANUEL123")
+        self.assertEqual(-3, self.validar)
+        
+    #Caso malicioso cuando la clave contiene un caracter especial
+    def test_claveConCaracEspecial(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Manuel123$","Manuel123$")
+        self.assertEqual(-3, self.validar)
+        
+    #Caso malicioso cuando la clave tiene menos de 3 letras
+    def test_claveMenosDe3Letras(self):
+        self.validar = self.prueba.registrarUsuario("hola@s.com", "Ma123456","Ma123456")
+        self.assertEqual(-3, self.validar)        
+
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
--- src/SistLogin/models.py
+++ src/SistLogin/models.py
@@ -8,7 +8,7 @@ class Seguridad:
         try:
             assert(re.match("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$",correo,flags=0)!=None)
         except:
-            print("Formato de correo no válido.")
+            print("Correo electrónico inválido.")
             return -1
         try:
             assert(clave1==clave2)
@@ -19,6 +19,7 @@ class Seguridad:
             minusculas = 0
             mayusculas = 0
             digitos = 0
+            string = ""
             assert(len(clave1) >= 8 and len(clave1) <= 16)
             for char in clave1:
                 if char.isdigit():
@@ -28,10 +29,25 @@ class Seguridad:
                 elif char.islower():
                     minusculas += 1
                 else:
+                    string = "No deben haber caracteres especiales en la clave."
                     raise
             assert(digitos >= 1 and mayusculas >= 1 and minusculas >= 1 and mayusculas + minusculas >= 3)
         except:
-            print("La clave no es valida")
+            if (string==""):
+                if (len(clave1) < 8):
+                    string = "La clave tiene menos de 8 caracteres"
+                elif (len(clave1) > 16):
+                    string = "La clave tiene mas de 16 caracteres"
+                else:
+                    if (digitos < 1):
+                        string = "La clave debe tener al menos un digito"
+                    elif (mayusculas < 1):
+                        string = "La clave debe tener al menos una letra mayuscula"
+                    elif (minusculas < 1):
+                        string = "La clave debe tener al menos una letra minuscula"
+                    elif(mayusculas+minusculas < 3):
+                        string = "La clave debe tener al menos tres letras"
+            print("Clave Inválida: " + string)
             return -3
         
         return 0
