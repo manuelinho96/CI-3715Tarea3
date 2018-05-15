66c073cd3f49b83a791be102106af5c5f4dc5c1e
invitado <invitado@e11.ac.labf.usb.ve>
2018-05-15 18:01:28 -0400

    Juan's commit

--- src/SistLogin/views.py
+++ src/SistLogin/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        #getting values from post
+        user = request.POST.get('username')
+        passwd = request.POST.get('pass')
+        template = loader.get_template('show.html')
+        context = {
+            'User': user,
+            'Paswd': passwd[::-1]
+        }
+        return HttpResponse(template.render(context,request))
+    else:
+        template = loader.get_template('SistLogin/index.html')
+        return HttpResponse(template.render(None,request))
